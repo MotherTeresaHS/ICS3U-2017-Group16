1ac3ee4ca4d70e0e0d79b39d83529aa65ab28d3a
MatthewWalsh2 <Matthew.walsh1@ocsbstudent.ca>
2018-01-11 15:55:59 -0500

    Added text and button images to game over and logo

--- game_over.py
+++ game_over.py
@@ -12,14 +12,44 @@ from game_scene import *
 class GameOverScene(Scene):
     def setup(self):
         # this method is called, when user moves to this scene
-        
         center_of_screen = self.size/2
-        
-        # add background color
         self.background = SpriteNode(position = self.size / 2, 
                                      color = 'blue', 
                                      parent = self, 
                                      size = self.size)
+        position_game_over_label = Vector2(self.size.x/2, self.size.y - 200)
+        self.game_over_label = LabelNode(text = 'Planet earth was destroyed!',
+                                         font = ('helvetica', 20),
+                                         parent = self,
+                                         position = position_game_over_label,
+                                         scale = 2.00)
+        self.game_over_label2 = LabelNode(text = 'Would you like to:',
+                                          font = ('helvetica', 20),
+                                          parent = self,
+                                          position = self.size/2)
+        play_again_label_position = Vector2(self.size.x * 0.66, 250)
+        self.play_again_label = LabelNode(text = 'play again',
+                                          font = ('helvetica', 20),
+                                          parent = self,
+                                          position = play_again_label_position)
+        return_to_menu_position = Vector2(self.size.x * 0.33, 250)
+        self.return_to_menu_label = LabelNode(text = 'return to main menu',
+                                              font = ('helvetica', 20),
+                                              parent = self,
+                                              position = return_to_menu_position)
+        play_again_button_position = Vector2(self.size.x * 0.66, 125)
+        self.play_again_button = SpriteNode('./assets/sprites/start.png',
+                                            parent = self,
+                                            position = play_again_button_position,
+                                            scale = 0.75)
+        back_button_position = Vector2(self.size.x * 0.33, 125)
+        self.back_button = SpriteNode('./assets/sprites/back_button.png',
+                                      parent = self,
+                                      position = back_button_position,
+                                      scale = 0.75)
+        
+        # add background color
+        
       
         
     def update(self):
@@ -36,7 +66,12 @@ class GameOverScene(Scene):
     
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
-        
+        #if self.play_again_button.frame.contains_point(touch.location):
+            #self.present_modal_scene(GameScene())
+            
+        # if start button is pressed, goto game scene
+        #if self.back_button.frame.contains_point(touch.location):
+            #self.present_modal_scene(MainMenuScene())
         pass
     
     def did_change_size(self):
